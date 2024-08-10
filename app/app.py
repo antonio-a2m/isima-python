21bc0eb9b2a2f6ee0f4e51d0e1748b0e9deebdb7
Antonio Gonzalez <aggarcia@gmail.com>
2024-08-10 10:40:17 -0600

    usar datos de github

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, json, render_template
+import githubClass
 app = Flask(__name__)
 
 @app.route('/', methods=('GET', 'POST'))
@@ -8,8 +9,19 @@ def home():
 
 @app.route('/github', methods=('GET', 'POST'))
 def github():
-    return render_template('index.html')
+    obj= githubClass.GithubClient()
+    userRes=obj.queryUser()
+    return userRes
     
+@app.route('/misdatos', methods=('GET', 'POST'))
+def githubdatos():
+    obj= githubClass.GithubClient()
+    userRes=obj.queryUser()
+    if not "name" in userRes:
+        return "<h2>El token no jala</h2>"
+    else:
+        html="<h2>{}</h2><h4>{}</h4><img src={}>".format(userRes["name"],userRes["bio"],userRes["avatar_url"])
+    return html
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
